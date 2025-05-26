30baac5770192f35f697365da368f9ce8bdbcd15
James Kamugisha <mjndahiro@gmail.com>
2025-05-26 07:31:13 -0400

    Added frontend UI

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,11 +4,12 @@ const {
   setGoal,
   updateGoal,
   deleteGoal,
-} = require("../contollers/goalController");
+} = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
